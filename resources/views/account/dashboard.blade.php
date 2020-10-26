a870f84219653ede2520d89881fab5c141024c44
Confidence Ugolo <ugoloconfidence@gmail.com>
2020-10-26 19:59:34 +0100

    Aimtoget public key

--- resources/views/account/dashboard.blade.php
+++ resources/views/account/dashboard.blade.php
@@ -161,7 +161,7 @@
             reference: '',
 
             // Merchant's aimotget PUBLIC KEY
-            key:'3684121b9a6cb6591d83c83f94f71d698f662a9228cb2285',
+            key: {{ env('AIMTOGEt_PUBLIC_KEY') }},
 
             //Url to the logo you want displayed on the payment modal
             logo_url: 'https://tebentutors.com/public/logo.png',
