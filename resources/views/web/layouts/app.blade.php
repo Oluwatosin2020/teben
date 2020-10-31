3ecb939dd743db3a3104a1c594c824fe721ae5b2
Confidence Ugolo <ugoloconfidence@gmail.com>
2020-10-31 11:57:52 +0100

    New images

--- resources/views/web/layouts/app.blade.php
+++ resources/views/web/layouts/app.blade.php
@@ -4,8 +4,10 @@
     <meta charset="utf-8">
     <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <title>Skill an Education Category Bootstrap Responsive Website Template - Home :: W3Layouts</title>
+    <meta name="description" content="Teben Tutors">
+    <meta name="keywords" content="teen tutor,tutor jobs, teben,tebentutors,teben tutors,tutors,lessons,home lesson,private lesson, children, education, teachers, hire teacher,tutorials">
+    <meta name="author" content="Confidence Ugolo">
+    <title>{{ $title ?? '' }} - Teben Tutors</title>
 
     <link href="http://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&amp;display=swap" rel="stylesheet">
     <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800&amp;display=swap" rel="stylesheet">
