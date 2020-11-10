a174f3f26d0ce510d97f0276292da78bcdb905c2
Confidence Ugolo <ugoloconfidence@gmail.com>
2020-11-10 13:02:26 +0100

    Debugging

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -56,6 +56,10 @@ class HomeController extends Controller
             return redirect()->route("home");
         }
 
+        dump($currentStatus);
+
+        dd($request->getMethod());
+
         if($request->getMethod() == "GET"){
             return view("user.profile.complete" , compact("currentStatus" , "user"));
         }
